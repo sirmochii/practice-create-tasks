542022ddfc70465814a545f7853331a8acd7266c
YemenFan <122573346+sirmochii@users.noreply.github.com>
2023-09-12 18:44:08 +0000

    Period 4 In class practice task

--- period4.py
+++ period4.py
@@ -1,1 +1,19 @@
+restaurants = ["Raising Canes", "Chick Fil A", "Taco Bell", "McDonalds", "Burger King"]
+
+print("Welsome to your resturant ranker! Your current ranking is ", restaurants,"! You can add and rank a new resturant")
+new_restaurant = input("what restaurant would you like to add?")
+
+def rank_restaurant(new_restaurant,restaurants):
+
+    for i in range(len(restaurants)):
+        str = "Do you you like A) ", new_restaurant, "or B) ", restaurants[i], "more? A/B "
+        ranking = input(str)
+        if ranking == "A":
+            restaurants.insert(i,new_restaurant)
+            break
+        elif ranking == "B":
+            continue
+
+    return restaurants
+print("you new ranking is", rank_restaurant(new_restaurant, restaurants))
 
